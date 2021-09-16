49a6dd3a0233fcbcc15e8380a4c32fd0efbaa84c
Jair <riuyagami@gmail.com>
2021-09-16 16:34:45 -0500

    Sidebar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,17 +1,17 @@
 <header class="header">
     <span class="header__btn-nav" id="btnNav">
-      <i class="header__icon fas fa-arrow-right"></i>@yield('title')
+      <i class="header__icon fas fa-bars"></i>@yield('title')
     </span>
 
-    <div class="d-flex">
+    {{-- <div class="d-flex">
       <input class="form-control me-2" type="search" placeholder="Buscar..." aria-label="Search">
       <button class="btn btn-outline-warning" type="submit"><i class="fas fa-search"></i></button>
-    </div>
+    </div> --}}
 
-    <div>
+    <div class="header__menu">
       <img class="header__admin-img" src="{{asset('img/avatars/avatar-2.jpg')}}">
       <span class="header__admin">
-        <span>Nombre Usuario</span><br>
+        <span>{{Auth::user()->name}}</span><br>
         <small>Rol de Usuario</small>
       </span>
       <i class="fas fa-chevron-down"></i>
--- resources/views/layout/navigation.blade.php
+++ resources/views/layout/navigation.blade.php
@@ -1,18 +1,47 @@
 <nav class="nav" id="nav">
     <div class="nav__content">
       <div class="nav__logo">
-        <img src="" alt="">
-        Botica Excelentemente
+        Botica
       </div>
 
       <ul class="nav__menu">
-        <li class="nav__list-title">Páginas</li>
-        <li class="nav__list"><a href="{{route('inicio.index')}}" class="nav__link {{ request()->routeIs('inicio.*') ? 'link--active' : '' }}"><i class="fas fa-bars"></i><span class="nav__link-text">Inicio</span></a></li>
-        <li class="nav__list"><a href="{{route('medicamentos.index')}}" class="nav__link {{ request()->routeIs('medicamentos.*') ? 'link--active' : '' }}"><i class="fas fa-bars"></i><span class="nav__link-text">Medicamento</span></a></li>
-        <li class="nav__list"><a href="{{route('articulos.index')}}" class="nav__link {{ request()->routeIs('articulos.*') ? 'link--active' : '' }}"><i class="fas fa-bars"></i><span class="nav__link-text">Articulos</span></a></li>
-        <li class="nav__list"><a href="{{route('stock.index')}}" class="nav__link {{ request()->routeIs('stock.*') ? 'link--active' : '' }}"><i class="fas fa-bars"></i><span class="nav__link-text">Stocks</span></a></li>
-        <li class="nav__list"><a href="{{route('ventas.index')}}" class="nav__link {{ request()->routeIs('ventas.*') ? 'link--active' : '' }}"><i class="fas fa-bars"></i><span class="nav__link-text">Ventas</span></a></li>
-        <li class="nav__list"><a  href="{{route('reportes.index')}}" class="nav__link {{ request()->routeIs('reportes.*') ? 'link--active' : '' }}"><i class="fas fa-bars"></i><span class="nav__link-text">Reportes</span></a></li>
+        {{-- <li class="nav__list-title">Páginas</li> --}}
+        <li class="nav__list">
+            <a href="{{route('inicio.index')}}" class="nav__link {{ request()->routeIs('inicio.*') ? 'link--active' : '' }}">
+                <i class="fas fa-home"></i>
+                <span class="nav__link-text">Inicio</span>
+            </a>
+        </li>
+        <li class="nav__list">
+            <a href="{{route('medicamentos.index')}}" class="nav__link {{ request()->routeIs('medicamentos.*') ? 'link--active' : '' }}">
+                <i class="fas fa-pills"></i>
+                <span class="nav__link-text">Medicamento</span>
+            </a>
+        </li>
+        <li class="nav__list">
+            <a href="{{route('articulos.index')}}" class="nav__link {{ request()->routeIs('articulos.*') ? 'link--active' : '' }}">
+                <i class="fas fa-shopping-cart"></i>
+                <span class="nav__link-text">Articulos</span>
+            </a>
+        </li>
+        <li class="nav__list">
+            <a href="{{route('stock.index')}}" class="nav__link {{ request()->routeIs('stock.*') ? 'link--active' : '' }}">
+                <i class="fas fa-cube"></i>
+                <span class="nav__link-text">Stocks</span>
+            </a>
+        </li>
+        <li class="nav__list">
+            <a href="{{route('ventas.index')}}" class="nav__link {{ request()->routeIs('ventas.*') ? 'link--active' : '' }}">
+                <i class="fas fa-dollar-sign"></i>
+                <span class="nav__link-text">Ventas</span>
+            </a>
+        </li>
+        <li class="nav__list">
+            <a  href="{{route('reportes.index')}}" class="nav__link {{ request()->routeIs('reportes.*') ? 'link--active' : '' }}">
+                <i class="fas fa-file-alt"></i>
+                <span class="nav__link-text">Reportes</span>
+            </a>
+        </li>
       </ul>
     </div>
   </nav>
